303a4d2cc8aaeac3fd40d1e576901ddaf838234b
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2021-05-11 10:30:35 +0300

    admin designs

---
+++ admin/index.php
@@ -0,0 +1,86 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/styles.css?v=<?php echo time(); ?>">
+    <title>blog Islam</title>
+</head>
+<body>
+
+ <section>
+   <div class="admin-tables">
+    <h3> All Posts  </h3>
+          <div class="blog-row">
+          <table>
+            <tr>
+              <td width="10%"> Post no: 1 <br> 02/04/2021</td>
+              <td width="40%"> Why are the man all pordige all ?</td>
+              <td width="10%"> <a href="">View </a> </td>
+              <td width="10%"> <a href="">Modify </a></td>
+              <td width="20%"><a href=""> Active</a> / <a href=""> Desactive</a></td>
+              <td width="10%"> <a href=""> Delete</a></td>
+            </tr>
+          </table>
+          </div>
+
+          <div class="blog-row">
+          <table>
+            <tr>
+              <td width="10%"> Post no: 1 <br> 02/04/2021</td>
+              <td width="40%"> Why are the man all pordige all ?</td>
+              <td width="10%"> <a href="">View </a> </td>
+              <td width="10%"> <a href="">Modify </a></td>
+              <td width="20%"><a href=""> Active</a> / <a href=""> Desactive</a></td>
+              <td width="10%"> <a href=""> Delete</a></td>
+            </tr>
+          </table>
+          </div>
+
+          <div class="blog-row">
+          <table>
+            <tr>
+              <td width="10%"> Post no: 1 <br> 02/04/2021</td>
+              <td width="40%"> Why are the man all pordige all ?</td>
+              <td width="10%"> <a href="">View </a> </td>
+              <td width="10%"> <a href="">Modify </a></td>
+              <td width="20%"><a href=""> Active</a> / <a href=""> Desactive</a></td>
+              <td width="10%"> <a href=""> Delete</a></td>
+            </tr>
+          </table>
+          </div>
+   </div>
+ </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </body>
+</html>
+    
